0b149efc72fa20380c2e2adfa0fb928804438491
jehuDedica <jehu.sesena@dedica.com.mx>
2023-08-04 06:04:48 -0600

    feature section's image was changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
                     </button>
                 </div>
             </nav>
-            <!-- Mobile menu, show/hide based on menu open state. -->
+            {{-- <!-- Mobile menu, show/hide based on menu open state. -->
             <div class="lg:hidden" role="dialog" aria-modal="true">
                 <!-- Background backdrop, show/hide based on slide-over state. -->
                 <div class="fixed inset-0 z-50"></div>
@@ -82,14 +82,14 @@
                                     class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
                             </div>
                             <div class="py-6">
-                                <a href="#"
+                                <a href="{{ route('login') }}"
                                     class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
                                     in</a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </header>
 
         <div class="relative isolate px-6 pt-14 lg:px-8">
@@ -103,7 +103,7 @@
                 <div class="hidden sm:mb-8 sm:flex sm:justify-center">
                     <div
                         class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                        Aprende sobre el cuidado del agua. <a href="#"
+                        Aprende sobre el cuidado del agua. <a href="https://psicologiaysalud.uv.mx/index.php/psicysalud/article/view/840/1548" target="_blank"
                             class="font-semibold text-indigo-600"><span class="absolute inset-0"
                                 aria-hidden="true"></span>Leer más <span aria-hidden="true">&rarr;</span></a>
                     </div>
@@ -112,9 +112,9 @@
                     <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Revisa las métricas de los usuarios de WaterEver</h1>
                     <p class="mt-6 text-lg leading-8 text-gray-600">Visualiza los avances, analiza información y estudia comportamientos</p>
                     <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="#"
+                        <a href="{{ route('login') }}"
                             class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Empezar</a>
-                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Ver más<span
+                        <a href="http://www.scielo.org.co/scielo.php?pid=S0123-77992019000200061&script=sci_arttext" target="_blank" class="text-sm font-semibold leading-6 text-gray-900">Ver proyecto parecido<span
                                 aria-hidden="true">→</span></a>
                     </div>
                 </div>
